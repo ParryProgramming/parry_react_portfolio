03f7a99c893cc908f8ad621251842f5388af0c7a
Parry <parryprogramming@gmail.com>
2024-06-09 17:29:31 -0400

    added image

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-// App.js
 import React from 'react';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
 import Project from './components/Project';
 import Footer from './components/Footer';
+import mainImage from '../src/images/profile.jpeg'; // replace with your image file path
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Navigation />
+      <img src={mainImage} alt="Main" /> {/* This line adds the image */}
       <Project />
       <Footer />
     </div>
