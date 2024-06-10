583190c5c010a34e14cd7ed4e074e01d4e4e1320
Parry <parryprogramming@gmail.com>
2024-06-10 01:05:54 -0400

    edited app.jsx routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-// App.jsx
 import React from 'react';
 import Header from './components/Header';
 import Navigation from './components/Navigation';
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
-import Contact from './components/Contact.jsx';
-import Footer from '../src/components/Footer.jsx';
-import Project from '../src/components/Project.jsx';
-import Header from '../src/components/Header.jsx';
-import Navigation from '../src/components/Navigation.jsx';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
